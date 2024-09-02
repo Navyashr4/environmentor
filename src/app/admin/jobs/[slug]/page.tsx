7dfecb943661abf58c02030a9944ec70b52af736
Navya Shree <navyashr4@gmail.com>
2024-09-02 15:42:45 -0500

    admin page jobpage

--- src/app/admin/jobs/[slug]/page.tsx
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -16,9 +16,8 @@ export default async function Page({ params: { slug } }: PageProps) {
 
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
-      I am hereeeee
-      {/* <JobPage job={job} />
-      <AdminSidebar job={job} /> */}
+      <JobPage job={job} />
+      {/* <AdminSidebar job={job} /> */}
     </main>
   );
 }
