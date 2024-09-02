9f96e628ba93c6e563b42f67e25fd78c359036f8
Navya Shree <navyashr4@gmail.com>
2024-09-02 15:44:59 -0500

    phoe nume

--- src/app/admin/jobs/[slug]/page.tsx
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -17,7 +17,7 @@ export default async function Page({ params: { slug } }: PageProps) {
   return (
     <main className="m-auto my-10 flex max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
       <JobPage job={job} />
-      {/* <AdminSidebar job={job} /> */}
+      <AdminSidebar job={job} />
     </main>
   );
 }
